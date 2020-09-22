b4e8035fa7ece9cfda3219d223da03d2fc4c72ed
Hrishabh Joshi <joshi008@users.noreply.github.com>
2020-09-22 23:52:39 +0530

    Update fetchWeather.js

--- src/fetchWeather.js
+++ src/fetchWeather.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const URL = "https://api.openweathermap.org/data/2.5/weather"
-
+const API_Key;
 
 export const fetchWeather = async (query) => {
     const { data } = await axios.get(URL, {
